2491504fb614cea1dd2fd33189175884a2319d56
Alexander Efremov <aedampir@gmail.com>
2014-10-29 17:29:18 +0800

    начал переписывать тест для проверки корректности рещения

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -15,21 +15,16 @@ protected:
     }
 
     virtual ~TestBase() {
-        // You can do clean-up work that doesn't throw exceptions here.
+
         memClean();
     }
 
-    // If the constructor and destructor are not enough for setting up
-    // and cleaning up each test, you can define the following methods:
-
     virtual void SetUp() {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
+
     }
 
     virtual void TearDown() {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
+
     }
 
     int GetSize() {
@@ -90,13 +85,13 @@ protected:
     }
 };
 
-class cputest : public TestBase {
+class cpu : public TestBase {
 protected:
 
-    cputest() {
+    cpu() {
     }
 
-    virtual ~cputest() {
+    virtual ~cpu() {
     }
 
     double *GetCpuToLevel(int level) {
@@ -107,67 +102,14 @@ protected:
 
 };
 
-//TEST_F(gputest, main_test)
-//{
+TEST_F(cpu, main_test) {
+    const int finishLevel = 1;
+    const int startLevel = 0;
 
-//	const int finishLevel = 7;
-//	const int startLevel = 0;
-//	const bool isComputeDiff = true;
-//	const bool isOneTl = false;
-//
-//	for (int level = startLevel; level < finishLevel; ++level)
-//	{
-//                std::cout << "level = " << level << std::endl;
-//		double *data = NULL;
-//
-//		ComputeParameters *p = new ComputeParameters(level, true, isComputeDiff);
-//		ASSERT_TRUE(p->result != NULL);
-//		std::cout << *p << std::endl;
-//
-//		float gpu_time = solve_at_gpu(p, isOneTl, isComputeDiff);
-//		ASSERT_TRUE(gpu_time != -1);
-//		if (isOneTl)
-//		{		data = GetCpuToLevel1TL(level, isComputeDiff); }
-//		else
-//		{  data = GetCpuToLevel(level, isComputeDiff); }
-//
-//		// data = _modelDataProvider.GetModelData(level);
-//
-//		printf("%s\n", "Checking is started");
-//		for (int i = 0; i < p->get_real_matrix_size(); i++)
-//		{
-//			ASSERT_NEAR(data[i], p->result[i], __FLT_EPSILON__) << "i = " <<  i << std::endl;
-//		}
-//                printf("%s\n\n", "Checking is done");
-//
-//		std::ostringstream oss; 
-//
-//		char *s = "diff_gpu_"; 
-//		oss << s << p->t_count << ".bin"; 
-//
-//		std::string name(oss.str());
-//
-//		if (isComputeDiff)
-//		{
-//			print_matrix_to_file(p->get_real_x_size(), p->get_real_y_size(), p->diff, name); 
-//		}
-//
-//		std::ostringstream oss1; 
-//		char *s1 = "gpu_result_"; 
-//		oss1 << s1 << p->t_count << ".bin"; 
-//		std::string name1(oss1.str());
-//
-//		print_matrix_to_file(p->get_real_x_size(), p->get_real_y_size(), p->result, name1); 
-//	
-//
-//		std::ostringstream oss2; 
-//		char *s2 = "cpu_result_"; 
-//		oss2 << s2 << p->t_count << ".bin"; 
-//		std::string name2(oss2.str());
-//
-//		print_matrix_to_file(p->get_real_x_size(), p->get_real_y_size(), data, name2); 
-//
-//		delete p;
-//		delete[] data;
-//	}
-//}
+    for (int level = startLevel; level < finishLevel; ++level) {
+        std::cout << "level = " << level << std::endl;
+        double *data = GetCpuToLevel(level);
+
+        delete[] data;
+    }
+}
