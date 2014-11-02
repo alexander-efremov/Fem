932a25fe6e5247200fa3052f1331b3c30866cc9d
Alexander Efremov <aedampir@gmail.com>
2014-11-02 18:27:14 +0800

    удалена compute_diff_write_to_file

--- include/utils.h
+++ include/utils.h
@@ -13,29 +13,6 @@ void print_matrix_to_file(int n, int m, double *data, std::string filename) {
     fclose(f);
 }
 
-/*void compute_diff_write_to_file(double *result, int tl, int n, int m, double tau)
-{
-    double c_h = 1. / n;
-    double *diff = new double[(n + 1) * (m + 1)]();
-
-    std::ostringstream oss;
-    oss << "diff_cpu_" << tl << ".bin";
-
-    std::string name(oss.str());
-
-    for (int j = 0; j <= m; j++)
-    {
-        for (int i = 0; i <= n; i++)
-        {
-            int opt = (n + 1) * j + i;
-            double f = analytical_solution(tl*tau, i*c_h, j * c_h);
-            diff [opt] = fabs(result[opt] - f);
-        }
-    }
-    print_matrix_to_file(n + 1, m + 1, diff, name);
-    delete[] diff;
-}*/
-
 void print_matrix(int n, int m, double *a, int precision = 8) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
